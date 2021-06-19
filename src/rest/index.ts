318dc548d2015da9c491ac428da7c9370d30e910
tpho216 <tpho216@gmail.com>
2021-06-19 21:51:50 +1000

    fix port issues

--- src/rest/index.ts
+++ src/rest/index.ts
@@ -2,7 +2,7 @@ const express = require('express');
 const db = require('./queries');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 4000;
 
 export const runDatabaseAPIService = () => {
   app.use(bodyParser.json());
@@ -22,6 +22,7 @@ export const runDatabaseAPIService = () => {
 
   app.listen(port, () => {
     console.log('App listnening on port ' + port);
+    debugger;
   });
 
 }
